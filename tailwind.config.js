c7e18e8a77d0a8a5ad05f13163bf45a66f705a1d
Soporte <operatons@colegiolacatolica.edu.pe>
2026-03-18 12:37:43 -0500

    empezando a diseñar el main para movil

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+export default {
+    content: [
+        "./index.html",
+        "./**/*.html",
+        "./**/*.js"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primaryBlue: "#1B5CFF",
+                primaryGreen: "#36A2BC",
+                primaryYellow: "#F4DA40",
+                textBlueDark: "hsl(223 60% 20%)",
+                backWhite: "hsl(200 10% 92%)",
+                textAtenuadoGris: "hsl(220 10% 45%)",
+                naranjaDestructive: "hsl(15 100% 61%)",
+                bordesGenerales: "hsl(210 15% 88%)",
+                capaOscuraHero: "hsl(223 80% 18%)",
+                colorWhatsapp: "hsl(136 100% 37%)",
+                beigeComplemento: "hsl(27 35% 61%)",
+                grisComplemnto: "hsl(204 6% 78%)"
+            }
+        }
+    },
+    plugins: [],
+}
